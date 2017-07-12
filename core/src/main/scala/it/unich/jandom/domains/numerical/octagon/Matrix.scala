7e9fa86c4c5f3a9831787628746ab09d9021de1e
Filippo Sestini <sestini.filippo@gmail.com>
2017-07-12 22:06:21 +0200

    Matrix: Change signature of method 'update'.

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Matrix.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Matrix.scala
@@ -7,7 +7,7 @@ trait Matrix[M[_]] // extends Functor[M] with Foldable[M] {
   extends Foldable[M] {
 
   def update[A](i: Int, j: Int, x: A)(m: M[A]): M[A]
-  def update[A](updater: (Int, Int) => Option[A])(m: M[A]): M[A]
+  def update[A](updater: (Int, Int) => A)(m: M[A]): M[A]
   def get[A](i: Int, j: Int)(m: M[A]): Option[A]
   def combine[A, B, C](f: (A, B) => C)(ma: M[A], mb: M[B]): M[C]
 }
