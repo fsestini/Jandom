aaa405d0e52a01a674315023e5ae8af9826bec7c
Filippo Sestini <sestini.filippo@gmail.com>
2017-07-13 17:33:09 +0200

    AbstractOctagon: Fix things.

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/AbstractOctagon.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/AbstractOctagon.scala
@@ -2,6 +2,7 @@ package it.unich.jandom.domains.numerical.octagon
 
 import it.unich.jandom.domains.numerical._
 import scala.language.higherKinds
+import scala.language.postfixOps
 import spire.math.Rational
 
 /**
@@ -13,24 +14,22 @@ import spire.math.Rational
   * Extremely temporary...
   */
 case class AbstractOctagon[M[_]](dbm: M[Double], e: DifferenceBoundMatrix[M]) {
-  def join(other: AbstractOctagon[M]): AbstractOctagon[M] =
+  def join(other: AbstractOctagon[M])(implicit lc: e.LatticeConstraint[Double])
+      : AbstractOctagon[M] =
     new AbstractOctagon(e.union(e.strongClosure(dbm), e.strongClosure(other.dbm)), e) // check if troo
 
-  def meet(other: AbstractOctagon[M]): AbstractOctagon[M] =
+  def meet(other: AbstractOctagon[M])(implicit lc: e.LatticeConstraint[Double]): AbstractOctagon[M] =
     new AbstractOctagon(e.intersection(dbm, other.dbm), e) // check if troo
 
   def forget(): AbstractOctagon[M] = ???
-  def forget(vi: VarIndex): AbstractOctagon[M]
-  def forgetDBM(vi: VarIndex, m: M[Double]): M[Double] = ???
-    // strongly close m, then apply forget operator
-
-  // etc...
+  def forget(vi: VarIndex): AbstractOctagon[M] = ???
+  def forgetDBM(vi: VarIndex, m: M[Double]): M[Double] = ??? // strongly close m, then apply forget operator
 
   def top = new AbstractOctagon(e.topDBM[Double], e: DifferenceBoundMatrix[M])
   def bottom = new AbstractOctagon(e.bottomDBM[Double], e: DifferenceBoundMatrix[M])
 
-  def toInterval: BoxDoubleDomain#Property
-  def fromInterval(box: BoxDoubleDomain#Property): AbstractOctagon[M]
+  def toInterval: BoxDoubleDomain#Property = ???
+  def fromInterval(box: BoxDoubleDomain#Property): AbstractOctagon[M] = ???
 
   private def forceOption[A](o: Option[A]): A = o match {
     case Some(x) => x
@@ -68,8 +67,8 @@ case class AbstractOctagon[M[_]](dbm: M[Double], e: DifferenceBoundMatrix[M]) {
   def singleConstantExactAssignment(v: VarIndex, const: Double)(dbm: M[Double])
       : M[Double] = {
     val f: (Int, Int) => Double = (i, j) =>
-      if (i == 2 * v - 1 && j == 2 * v) (- 2 * const) else
-        if (i == 2 * v && j == 2 * v - 1) (2 * const) else
+      if (i == 2 * v - 1 && j == 2 * v) -2 * const else
+        if (i == 2 * v && j == 2 * v - 1) 2 * const else
           forceOption(e.get(i, j)(forgetDBM(v, dbm)))
     e.update(f)(dbm)
     // new AbstractOctagon(e.update(f)(dbm), e)
@@ -84,14 +83,13 @@ case class AbstractOctagon[M[_]](dbm: M[Double], e: DifferenceBoundMatrix[M]) {
                (j != 2 * v - 1 && j != 2 * v && i == 2 * v)
       val g3 = i == 2 * v - 1 && j == 2 * v
       val g4 = i == 2 * v && j == 2 * v - 1
-      if (g1) (forceOption(e.get(i,j)(dbm)) - const) else
-        if (g2) (forceOption(e.get(i,j)(dbm)) + const) else
-          if (g3) (forceOption(e.get(i,j)) - 2 * const) else
-            if (g4) (forceOption(e.get(i, j)(dbm)) + 2 * const) else
+      if (g1) forceOption(e.get(i, j)(dbm)) - const else
+        if (g2) forceOption(e.get(i, j)(dbm)) + const else
+          if (g3) forceOption(e.get(i, j)(dbm)) - 2 * const else
+            if (g4) forceOption(e.get(i, j)(dbm)) + 2 * const else
               forceOption(e.get(i, j)(dbm))
     }
     e.update(f)(dbm)
-    // new AbstractOctagon(e.update(f)(dbm), e)
   }
 
   def doublePositiveExactAssignment(v: VarIndex, other: VarIndex, const: Double)
@@ -114,32 +112,30 @@ case class AbstractOctagon[M[_]](dbm: M[Double], e: DifferenceBoundMatrix[M]) {
   // x := - x
   def singleNegativeZeroAssignment(v: VarIndex)(dbm: M[Double]): M[Double] = {
     val f: (Int, Int) => Double = (i, j) => {
-      if (i == 2 * v - 1 || i == 2 * v) (
-        if (j == 2 * v - 1 || j ==  2 * v)
-          e.get(signed(i), signed(i))(dbm)
+      if (i == 2 * v - 1 || i == 2 * v) {
+        if (j == 2 * v - 1 || j == 2 * v)
+          forceOption(e.get(signed(i), signed(i))(dbm))
         else
-          e.get(signed(i), j)(dbm)
-      )
-      else (
-        if (j == 2 * v - 1 || j ==  2 * v)
-          e.get(i, signed(j))(dbm)
+          forceOption(e.get(signed(i), j)(dbm))
+      } else {
+        if (j == 2 * v - 1 || j == 2 * v)
+          forceOption(e.get(i, signed(j))(dbm))
         else
-          e.get(i, j)(dbm)
-      )
+          forceOption(e.get(i, j)(dbm))
+      }
     }
     e.update(f)(dbm)
-    // new AbstractOctagon(e.update(f)(dbm), e)
   }
 
   // x := - y
   def doubleNegativeZeroAssignment(v: VarIndex, other: VarIndex): M[Double] => M[Double] =
     doublePositiveExactAssignment(v, other, 0) _ andThen
-      singleNegativeZeroAssignment(v) _
+      singleNegativeZeroAssignment(v)
 
   // x := - x + c
   def singleNegativeExactAssignment(v: Int, const: Double): M[Double] => M[Double] =
     singleNegativeZeroAssignment(v) _ andThen
-      singlePositiveExactAssignment(v, const) _
+      singlePositiveExactAssignment(v, const)
 
   // x := - y + c
   def doubleNegativeExactAssignment(v: Int, other: VarIndex, const: Double): M[Double] => M[Double] =
@@ -153,7 +149,11 @@ case class AbstractOctagon[M[_]](dbm: M[Double], e: DifferenceBoundMatrix[M]) {
     lf + vlf
   }
 
-  private def inverseLf(lf: LinearForm): LinearForm = lf * (-1)
+  private def varLf(v: VarIndex, dimension: Int): LinearForm = {
+    val sss: Seq[Rational] =
+      (0 to dimension).map(x => if (x == v + 1) Rational(1) else Rational(0))
+    new DenseLinearForm(sss)
+  }
 
   def thruIntervals(v: VarIndex, lf: LinearForm)(dbm: M[Double]): M[Double] = {
     val f: (Int, Int) => Double = (i, j) => {
