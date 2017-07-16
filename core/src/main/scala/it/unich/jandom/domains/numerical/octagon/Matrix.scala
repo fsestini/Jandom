c3294ebe09b96c5fa4f723bda9f1ee3a83685cc6
WalkerTR <dal.bianco.davide@gmail.com>
2017-07-16 12:18:03 +0200

    Change semantic of Matrix: access out of bounds throws exception

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Matrix.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Matrix.scala
@@ -8,6 +8,6 @@ trait Matrix[M[_]] // extends Functor[M] with Foldable[M] {
 
   def update[A](i: Int, j: Int, x: A)(m: M[A]): M[A]
   def update[A](updater: (Int, Int) => A)(m: M[A]): M[A]
-  def get[A](i: Int, j: Int)(m: M[A]): Option[A]
+  def get[A](i: Int, j: Int)(m: M[A]): A
   def combine[A, B, C](f: (A, B) => C)(ma: M[A], mb: M[B]): M[C]
 }
