da8d15d826f6ab42b13d4d52c6edbd3236cab26e
Filippo Sestini <sestini.filippo@gmail.com>
2017-07-17 18:05:46 +0200

    FastDBM: Fix skeleton strong closure algorithm for decomposed matrices.

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/FastDBM.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/FastDBM.scala
@@ -114,28 +114,42 @@ case class SparseDBM[M[_], A](m: M[A], rdbm: DenseSparseDBM[M]) extends FastDBM[
 // NNI for decomposed DBMs is computed as the sum of its submatrices' NNI.
 case class DecomposedDBM[M[_], A](completeDBM: M[A],
                                   indepComponents: Seq[Seq[VarIndex]],
-                                  rdbm: DenseSparseDBM[M])
-    extends FastDBM[M, A] {
+                                  rdbm: DenseSparseDBM[M]) extends FastDBM[M, A] {
 
+  // Skeleton of strong closure for decomposed matrices.
   def decStrongClosure(m: DecomposedDBM[M, A])
-      (implicit ifield: InfField[A]): CFastDBM[M, Closed, A] = {
-    val subs = indepComponents.map(seq => rdbm.extract(seq)(completeDBM))
-    val closedSubs = subs.map(m => rdbm.strongClosure(m))
-    val actualClosedSubs = Applicative[Option].sequence(closedSubs)
-    val (newM, newNNI, newIs) = closedSubs.foldRight((completeDBM, 0, indepComponents))(
-      (x, y) => for {
-        (sub, NNI(nni), _) <- x
-        (full, fullNNI, is) <- y
-      } yield (rdbm.pour(sub)(full), nni + fullNNI, is)
-      )
-      // (x, y) => (x, y) match {
-      //   case ((sub, NNI(nni), _), (full, fullNNI, is)) =>
-      //     (rdbm.pour(sub)(full), nni + fullNNI, is)
-      // })
-    val actualNewIndepComponents = ???
-    (DecomposedDBM[M, A](newM, newIs, rdbm), NNI(newNNI), newIs)
+    (implicit ifield: InfField[A]):
+      Option[(CFastDBM[M, Closed, A], NNI, List[List[VarIndex]])] = {
+    val subs: Seq[M[A]] =
+      indepComponents.map(seq => rdbm.extract(seq)(completeDBM))
+    val closedSubs: List[Option[(M[A], NNI, List[List[VarIndex]])]] =
+      subs.map(m => rdbm.strongClosure(m)).toList
+    val actualClosedSubs: Option[List[(M[A], NNI, List[List[VarIndex]])]] =
+      Applicative[Option].sequence(closedSubs)
+    actualClosedSubs match {
+      case Some(cloSu) => {
+        val (newM, newNNI, newIs) = cloSu.foldRight((completeDBM, 0, indepComponents))(
+          (x, y) => (x, y) match {
+            case ((sub, NNI(nni), _), (full, fullNNI, is)) =>
+              (rdbm.pour(sub)(full), nni + fullNNI, is)})
+        // Computation of the new list of independent components is non-trivial.
+        val actualNewIndepComponents = ???
+        val m: CFastDBM[M, Closed, A] =
+          if (Lol.nuffDecomposed(actualNewIndepComponents)) {
+            CFast(DecomposedDBM(newM, actualNewIndepComponents, rdbm))
+          } else if (Lol.nuffSparse(NNI(newNNI))) {
+            // might be that we have to compute the index of non-infinite terms
+            // during the strong closure above, and pass them to the
+            // sparse dbm constructor.
+            CFast(SparseDBM(newM, rdbm))
+          } else {
+            CFast(DenseDBM(newM, rdbm))
+          }
+        Some((m, NNI(newNNI), actualNewIndepComponents))
+      }
+      case None => None
+    }
   }
-
 }
 
 // The Top type can be seen as a degenerate case of the decomposed one with
