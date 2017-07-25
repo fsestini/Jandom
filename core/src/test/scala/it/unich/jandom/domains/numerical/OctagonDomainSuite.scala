bfcdb2417392e354f9511a99a63ea5cf42b88d50
Tobia Tesan <tobia.tesan@gmail.com>
2017-07-25 12:50:31 +0200

    OctagonDomain: Add some [as of yet failing] tests for the octagon domain

--- core/src/test/scala/it/unich/jandom/domains/numerical/OctagonDomainSuite.scala
+++ core/src/test/scala/it/unich/jandom/domains/numerical/OctagonDomainSuite.scala
@@ -16,111 +16,392 @@
   * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
   */
 
-package it.unich.jandom.domains.numerical
-
+package it.unich.jandom.domains.numerical.octagon
+import it.unich.jandom.domains.numerical._
 import org.scalatest.FunSuite
 
+
 class OctagonDomainSuite extends FunSuite {
-  lazy val dom = new OctagonDomain()
-  import octagon._
 
-  sealed abstract class DummyDbm[+A,B](){}
+  val e = FunDBMInstance.funDBM
+  val em =  FunMatrixMatrixInstance.funMatrixIsMatrix
+  val inf = Double.PositiveInfinity
+  val dom = new OctagonDomain[FunDBM](e)
 
-  case class RegularDummyDbm[+A, B]
-    (val arr : Option[Seq[Seq[B]]],
-      val state : A,
-      val nOfVars : Int)
-      extends DummyDbm[A,B]
-  case class TopDummyDbm[B] (
-    val nOfVars : Int) extends  DummyDbm[Closed ,B]
-  case class BottomDummyDbm[B] (
-    val nOfVars : Int) extends  DummyDbm[Closed ,B]
+  def elementwiseEquals(len : Int)(m : FunMatrix[Double], expected : Array[Array[Double]], extra : String = "") = {
+    def prettyM(len : Int)(dbm : FunMatrix[Double]) : String =
+      (0 to len - 1).map((i) => (0 to len - 1).map((j) => em.get(i,j)(dbm)).mkString(",")).mkString("\n")
 
-  val e : DifferenceBoundMatrix[DummyDbm] = new DifferenceBoundMatrix[DummyDbm] {
-    def get[B](i: Int, j: Int)(m: DummyDbm[DBMState, B]): Option[B] = m match {
-      case m : RegularDummyDbm[_,_] => Some(m.arr.get(i)(j))
-      case _ => ???
-    }
+    def prettyArray(len : Int)(array : Array[Array[Double]]) : String =
+      (0 to len - 1).map((i) => (0 to len - 1).map((j) => array(i)(j)).mkString(",")).mkString("\n")
 
-    def update[B](f: (Int, Int) => B)(m: DummyDbm[DBMState, B]): DummyDbm[DBMState, B] = m match {
-      case m : RegularDummyDbm[DBMState,B] => {
-        m.copy(arr =
-          Some(
-            (0 to m.arr.get.size-1).map(
-              (i) => (0 to (m.arr.get)(i).size-1).map(
-                (j) => f(i,j)
-              )
-            )
-          )
+    for (i <- 0 to len - 1)
+      for (j <- 0 to len - 1)
+        assert(em.get(i,j)(m) == expected(i)(j),
+          "Mismatch at index ("+i+", "+j+"), M:\n"
+          + prettyM(len)(m)
+          + "\n did not equal: \n"
+          + prettyArray(len)(expected)
+          + "\nExtra info: "
+          +extra
         )
+  }
+
+  test ("FunMatrix sanity check") {
+    def f (i : Int, j : Int) : Double = ((i + j) % 2)
+    val m = FunMatrix[Double](f, 4)
+    assert(m(0,0) == 0)
+    assert(m(0,1) == 1)
+    assert(m(3,3) == 0)
+    def g (i : Int, j : Int) : Double = ((i + j + 1) % 2)
+    val n = m.update(g)
+    assert(n(0,0) == 1)
+    assert(n(0,1) == 0)
+    def h(a : Double, b : Double) = a + b
+    val o = m.combine(n, h)
+    assert(o(0,0) == 1)
+    assert(o(0,1) == 1)
+    assert(o.toList == List(1,1,1,1,
+      1,1,1,1,
+      1,1,1,1,
+      1,1,1,1))
+  }
+
+  test ("FunMatrixIsMatrix sanity check") {
+    def f (i : Int, j : Int) : Double = ((i + j) % 2)
+    def g (i : Int, j : Int) : Double = ((i + j + 1) % 2)
+    def h(a : Double, b : Double) = a + b
+    def i (x : Double, z : => Double) = (x + z)
+    val m = FunMatrix[Double](f, 4)
+    val n = FunMatrix[Double](g, 4)
+    val fourfours = (em.pure(2, 4.0))
+    assert((em.combine(h)(m,n))(0,0) == 1)
+    assert(fourfours.toList == List(4,4,4,4))
+    assert(em.foldRight(fourfours, 0.0)(i) == 16)
+  }
+
+  test ("fromFun sanity check") {
+    // These are fig 9a and b from Mine01, fig9b is the closure of fig9a
+    val fig9a = Array(
+      Array(0,   inf, inf, inf),
+      Array(inf,   0,   3, inf),
+      Array(inf, inf, inf, inf),
+      Array(3,   inf, inf, inf)
+    )
+
+    val fig9b = Array(
+      Array(0,   inf, inf, inf),
+      Array(2,   0,   3,   inf),
+      Array(inf, inf, inf, inf),
+      Array(3,   inf, 4,   inf)
+    )
+    val e = FunMatrixMatrixInstance.funMatrixIsMatrix
+    val e1 = FunDBMInstance.funDBM
+    val funm9a = FunMatrix((i : Int, j : Int) => (fig9a(i))(j), 4)
+    val funm9b = FunMatrix((i : Int, j : Int) => (fig9b(i))(j), 4)
+    val dbm9b = ClosedFunDBM(funm9b)
+    val fromFun = FunDBMInstance.funDBM.fromFun(4, (i : Int, j : Int) => (fig9a(i))(j))
+    /*
+     * Now, fromFun should be
+     * 1. closed
+     * 2. should look like dbm9b elementwise
+     * 3. probably should also == dbm9b
+     */
+    fromFun match {
+      case x : ClosedFunDBM[Double] => {
+        elementwiseEquals(4)(x.m, fig9b)
       }
-      case _ => ???
+      case _ => assert(false, "Not a ClosedFunDBM")
     }
+    assert(fromFun == dbm9b)
+    // TODO Not sure if this last one is really the desired semantics, double-check
+  }
 
-    def strongClosure[B](m: DummyDbm[DBMState, B])
-      (implicit evidence: InfField[B]): DummyDbm[Closed, B] = m match {
-      case m : RegularDummyDbm[_,_] => m.copy(state = Closed())
-      // Unsound! For testing only
-      case m : TopDummyDbm[B] => m
-      case m : BottomDummyDbm[B] => m
-    }
+  test ("strongClosure sanity check") {
+    // O4 and O4* from www.srl.inf.ethz.ch/pa2015/Octagon.pdf
+    val o4 = Array(
+      Array(0, -2,  0, -2,  inf,  inf),
+      Array(2,  0,  2,  0,  inf,    0),
+      Array(0, -2,  0, -2,  inf,  inf),
+      Array(2,  0,  2,  0,  inf,  inf),
+      Array(0, inf, inf, inf, 0, inf),
+      Array(inf, inf, inf, inf, inf, 0)
+    )
 
-    def incrementalClosure[B](v: VarIndex)(m: DummyDbm[DBMState, B])
-      (implicit evidence: InfField[B]): DummyDbm[Closed, B] =  ???
+    val o4dbm : ClosedFunDBM[Double] = ClosedFunDBM(
+      FunMatrix((i : Int, j : Int) => o4(i)(j), 6)
+    )
 
-    def bottomDBM[B](nOfVars : Int) : DummyDbm[Closed, B] =
-      new BottomDummyDbm[B](nOfVars)
+    val expectedo4star = Array[Array[Double]](
+      Array(0, -2, 0, -2, inf, -2),
+      Array(2,  0, 2,  0, inf,  0),
+      Array(0, -2, 0, -2, inf, -2),
+      Array(2,  0, 2,  0, inf,  0),
+      Array(0, -2, 0, -2,   0, -2),
+      Array(inf, inf, inf, inf, inf, 0)
+    )
 
-    def isBottomDBM[B](m: DummyDbm[DBMState, B]): Boolean = m match {
-      case m : BottomDummyDbm[B] => true
-      case _ => false
-    }
+    val o4star = e.strongClosure(o4dbm)
 
-    def topDBM[B](nOfVars : Int): DummyDbm[Closed, B] =
-      new TopDummyDbm[B](nOfVars)
-
-    def dbmUnion[S <: DBMState, B](m1: DummyDbm[S, B], m2: DummyDbm[S, B])
-      (implicit ifield: InfField[B]): DummyDbm[S, B] = ???
-
-    def dbmIntersection[B](m1: DummyDbm[DBMState, B], m2: DummyDbm[DBMState, B])
-      (implicit ifield: InfField[B]): DummyDbm[DBMState, B] = ???
-
-    def flipVar[S <: DBMState, B](v: VarIndex)
-      (m: DummyDbm[S, B])(implicit ifield: InfField[B]): DummyDbm[S, B] = ???
-
-    def addScalarOnVar[S <: DBMState, B](v: VarIndex, c: B)
-      (m: DummyDbm[S, B])(implicit ifield: InfField[B]): DummyDbm[S, B] = ???
-
-    def forget[S <: DBMState, B](v: VarIndex)(m: DummyDbm[S, B]): DummyDbm[S, B]  = ???
-
-    def nOfVars[B](m: DummyDbm[DBMState, B]): Int = m match {
-      case m : RegularDummyDbm[_,_] => m.nOfVars
-      case m : TopDummyDbm[_] => m.nOfVars
-      case m : BottomDummyDbm[_] => m.nOfVars
+    o4star match {
+      case x : ClosedFunDBM[Double] => elementwiseEquals(6: Int)(x.m, expectedo4star)
+      case _ => assert(false)
     }
   }
 
-  test("Test singleton interval") {
-    val arr = Some(Seq(Seq(0,-246.0),Seq(246.0,0)))
-    val foo = new RegularDummyDbm[Closed, Double](arr, Closed(), 1)
-    val ao = AbstractOctagon[DummyDbm](foo, e)
-    assert(ao.toInterval.low(0) == ao.toInterval.high(0))
-    assert(ao.toInterval.low(0) == 123)
+
+  test ("T {v0 <- 2}.toInterval == [2,2]") {
+    val a = AbstractOctagon(e.topDBM[Double](1), dom, e)
+    val c = LinearForm.c(2)
+    val b = a.linearAssignment(0, c)
+    assert(b.toInterval.high.head == 2)
+    assert(b.toInterval.low.head == 2)
   }
 
-  test("Test join") {
-    val arr = Some(Seq(Seq(0,-220.0),Seq(220.0,0)))
-    val arr2 = Some(Seq(Seq(0,-200.0),Seq(100.0,0)))
-    val foo = new RegularDummyDbm[Closed, Double](arr, Closed(), 1)
-    val foo2 = new RegularDummyDbm[Closed, Double](arr2, Closed(), 1)
-    val ao = AbstractOctagon[DummyDbm](foo, e)
-    val ao2 = AbstractOctagon[DummyDbm](foo2, e)
-    assert(ao.toInterval.low(0) == 110)
-    assert(ao.toInterval.high(0) == 110)
-    assert(ao2.toInterval.low(0) == 100)
-    assert(ao2.toInterval.high(0) == 100)
-    val join = ao.join(ao2)
-    assert(join.toInterval.low(0) == 100)
-    assert(join.toInterval.high(0) == 110)
+  test ("Union of [1,1], [2, 2] == [1, 2]") {
+    val a = AbstractOctagon(e.topDBM[Double](1), dom, e)
+    val c2 = LinearForm.c(1)
+    val c1 = LinearForm.c(2)
+    val b1 = a.linearAssignment(0, c1)
+    val b2 = a.linearAssignment(0, c2)
+    val union = b1 union b2
+    assert(union.toInterval.isEmpty == false)
+    assert(union.toInterval.high.head == 2)
+    assert(union.toInterval.low.head == 1)
+  }
+
+  test ("Intersection of [1,1], [2,2] is empty") {
+    val a = AbstractOctagon(e.topDBM[Double](1), dom, e)
+    val c2 = LinearForm.c(1)
+    val c1 = LinearForm.c(2)
+    val b1 = a.linearAssignment(0, c1)
+    val b2 = a.linearAssignment(0, c2)
+    val intersection = b1 intersection b2
+    assert(intersection.toInterval.isEmpty == true)
+  }
+
+  test ("[1,2] <= [0,3]") {
+    val a = AbstractOctagon(e.topDBM[Double](1), dom, e)
+    val c1 = LinearForm.c(1);  val c2 = LinearForm.c(2)
+    val c3 = LinearForm.c(0);  val c4 = LinearForm.c(3)
+    val b1 = a.linearAssignment(0, c1)
+    val b2 = a.linearAssignment(0, c2)
+    val i1 = b1 union b2 // i1 = [1,2]
+    val b3 = a.linearAssignment(0, c3)
+    val b4 = a.linearAssignment(0, c4)
+    val i2 = b3 union b4 // i2 = [0,3]
+    assert (i1 <= i2)
+  }
+
+  test ("Sanity check for decodeTest") {
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(0,0,0,0))
+        // TODO Maybe this case is a special case of one of the others?
+        // If so which one?
+    ) match {
+      case Fallback() => assert(true)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(1,1,0,0))
+    ) match {
+      case Case1Test(vl, c) => assert(vl == 0 & c == c)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(4,1,0,0))
+    ) match {
+      case Case1Test(vl, c) => assert(vl == 0 & c == c)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(0,1,0,0))
+    ) match {
+      case Case1Test(vl, c) => assert(vl == 0 & c == c)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(0,123,0,0))
+    ) match {
+      case Fallback() => assert(true)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(1,0,-1,0))
+    ) match {
+      case Case2Test(vl, c) => assert(vl == 1 & c == 1)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(1,1,0,-1))
+    ) match {
+      case Case3Test(vl, vk, c) => assert(vl == 0 & vk == 2 & c == 1)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(1,1,0,1))
+    ) match {
+      case Case4Test(vl, vk, c) => assert(
+        (vl == 0 & vk == 2 & c == 1)
+          |
+        (vl == 2 & vk == 0 & c == 1)
+      )
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(1,-1,0,-1))
+    ) match {
+      case Case5Test(vl, vk, c) => assert(vl == 0 & vk == 2 & c == 1)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    AbstractOctagon.decodeTest(
+      new DenseLinearForm(Seq(1,-1,1,-1))
+    ) match {
+      case Fallback() => assert(true)
+      case (other) => assert(false, "Got " + other)
+    }
+  }
+
+  test ("Sanity check for fallbackUpdate") {
+    // CAVEAT: Super trivial example, not nearly enough to trust the thing
+    val topoct = AbstractOctagon(e.topDBM[Double](6), dom, e)
+    val assigned =
+      topoct.linearAssignment(0, LinearForm.c(1))
+      .linearAssignment(1, LinearForm.c(2))
+      .linearAssignment(2, LinearForm.c(3))
+    // ie assign (1,2,3) to (v1, v2, v3)
+    // now we assign Oct -= (v1 + v2 + v3), i.e we have
+    // e = lf(0,1,1,1)
+    val expectedassignment : Array[Array[Double]] = Array(
+      Array(0,   -2,   1, -3,   2, -4),
+      Array(2,    0,   3, -1,   4, -2),
+      Array(-1,  -3,   0, -4,   1, -5),
+      Array( 3,   1,   4,  0,   5, -1),
+      Array(-2,  -4,  -1, -5,   0, -6),
+      Array( 4,   2,   5,  1,   6,  0)
+    )
+
+    assigned.dbm match {
+      case x : ClosedFunDBM[Double] =>  elementwiseEquals(6)(x.m, expectedassignment)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    val updated = assigned.fallbackUpdate(
+      new DenseLinearForm(Seq(0,1,1,1))
+    )
+
+    val expected : Array[Array[Double]] = Array(
+      Array(0,  -10,  -5, -3,  -4, -4),
+      Array(2,    0,  -9, -5, -10, -4),
+      Array(-7,  -3,   0, -8,  -5, -5),
+      Array(-9,  -7,   4,  0, -11, -5),
+      Array(-8,  -4,  -7, -5,   0, -6),
+      Array(-10, -8, -11, -7,   6,  0)
+    )
+
+    def peek[S, A](m: FunDBM[S, A]): Unit = m match {
+      case dbm : ClosedFunDBM[Double] => elementwiseEquals(6)(dbm.m, expected)
+      case _ => assert(false)
+    }
+    peek(updated.elem)
+  }
+
+  test ("FunDBM-based AbstractOctagon matches example from www.srl.inf.ethz.ch/pa2015/Octagon.pdf p. 24") {
+    /*
+     * x <- 1
+     * y <- x
+     * while (x <= m)
+     *   x <- x + 1
+     *   y <- y + x
+     * assert(y >= m)
+     */
+    val o1 = AbstractOctagon(e.topDBM[Double](6), dom, e)
+
+    // We choose (v0, v1, v2) = (x, y, m)
+    // x <- 1
+    val o2star = o1.linearAssignment(0, LinearForm.c(1))
+    val expectedo2star = Array(
+      Array(0, -2, inf, inf, inf, inf),
+      Array(2,  0, inf,  inf, inf, inf),
+      Array(inf, inf, 0, inf, inf, inf),
+      Array(inf,  inf, inf,  0, inf, inf),
+      Array(inf, inf, inf, inf, 0, inf),
+      Array(inf, inf, inf, inf, inf, 0)
+    )
+    o2star.dbm match {
+      case dbm : ClosedFunDBM[Double] => elementwiseEquals(6)(dbm.m, expectedo2star)
+      case _ => assert(false)
+    }
+
+    // y <- x
+    val o3star = o2star.linearAssignment(1, new DenseLinearForm(Seq(0,1,0,0)))
+    val expectedo3star = Array(
+      Array(0, -2, 0, -2, inf, inf),
+      Array(2,  0, 2,  0, inf, inf),
+      Array(0, -2, 0, -2, inf, inf),
+      Array(2,  0, 2,  0, inf, inf),
+      Array(inf, inf, inf, inf, 0, inf),
+      Array(inf, inf, inf, inf, inf, 0)
+    )
+
+    o3star.dbm match {
+      case x : ClosedFunDBM[Double] => elementwiseEquals(6)(x.m, expectedo3star)
+      case _ => assert(false)
+    }
+
+    // (x <= m) ==> (x - m <= 0) == (v0 - v2 <= 0)
+
+    val form = AbstractOctagon.decodeTest(new DenseLinearForm(Seq(0,1,0,-1)))
+    form match {
+      case Case3Test(vi, vj, c) => assert(vi == 0 & vj == 2)
+      case (other) => assert(false, "Got " + other)
+    }
+
+    // First we check that the actual computation is correct...
+    val o4 = o3star.linearInequalityEx(new DenseLinearForm(Seq(0,1,0,-1)))
+    val expectedo4 = Array(
+       Array(0, -2, 0, -2, inf, inf),
+       Array(2,  0, 2,  0, inf, 0),
+       Array(0, -2, 0, -2, inf, inf),
+       Array(2,  0, 2,  0, inf, inf),
+       Array(0, inf, inf, inf, 0, inf),
+       Array(inf, inf, inf, inf, inf, 0)
+    )
+
+    def peek[S, A](m: FunDBM[S, A]): Unit = m match {
+      case dbm : ClosedFunDBM[Double] => elementwiseEquals(6)(dbm.m, expectedo4, "Checking that o4 == expectedo4, is ClosedFunDBM")
+      case dbm : NonClosedFunDBM[Double] => elementwiseEquals(6)(dbm.m, expectedo4, "Checking that o4 == expectedo4, is NonClosedFunDBM")
+      case _ => assert(false)
+    }
+    peek(o4.elem)
+
+    // Then we check that linearEquality takes care of the closure
+    val o4star = o3star.linearInequality(new DenseLinearForm(Seq(0,1,0,-1)))
+    val expectedo4star = Array(
+      Array(0, -2, 0, -2, inf, -2),
+      Array(2,  0, 2,  0, inf, 0),
+      Array(0, -2, 0, -2, inf, -2),
+      Array(2,  0, 2,  0, inf, 0),
+      Array(inf, -2, 0, -2, 0, -2),
+      Array(inf, inf, inf, inf, inf, 0)
+    )
+    o4star.dbm match {
+      case dbm : ClosedFunDBM[Double] => elementwiseEquals(6)(dbm.m, expectedo4star, "Checking that o4star == expectedo4star")
+      case _ => assert(false)
+    }
+
+    // If it's really closed it should be also a fixed point for the closure op?
+    assert(e.strongClosure(o4star.dbm) == o4star.dbm)
+
+   // TO BE CONTINUED...
   }
 }
---
+++ core/src/test/scala/it/unich/jandom/domains/numerical/OctagonSpecification.scala
@@ -0,0 +1,241 @@
+/**
+  * Copyright 2017 Davide Dal Bianco, Filippo Sestini, Tobia Tesan
+  *
+  * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+  * JANDOM is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * JANDOM is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of a
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+package it.unich.jandom.domains.numerical.octagon
+import it.unich.jandom.domains.numerical._
+import org.scalatest.PropSpec
+import org.scalatest.prop.{PropertyChecks, Checkers}
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary
+import Arbitrary._
+
+class OctagonSpecification extends PropSpec with PropertyChecks {
+  val e = FunDBMInstance.funDBM
+  val oct = new OctagonDomain(e)
+  val box = BoxDoubleDomain(false)
+
+  def GenSmallInt : Gen[Int] = Gen.choose(1, 32)
+
+  def GenOrderedPair : Gen[(Double, Double)] = for {
+    low <- arbitrary[Double]
+    high <- Gen.choose(low, Double.MaxValue)
+  } yield (low, high)
+
+  def GenOrderedDistinctPair : Gen[(Double, Double)] = for {
+    low <- arbitrary[Double]
+    high <- Gen.choose(low, Double.MaxValue).suchThat(_ > low)
+  } yield (low, high)
+
+  implicit def arbBox : Arbitrary[box.Property] =
+    Arbitrary {
+
+      for {
+        n <- Gen.choose(1,20)
+        pairs : Array[(Double, Double)] <- Gen.containerOfN[Array, (Double, Double)](n, GenOrderedPair)
+      } yield (new box.Property(pairs.unzip._1, pairs.unzip._2, false))
+    }
+
+  property("Can create from interval") {
+    forAll {
+      (b1: box.Property)
+      =>
+      val a = AbstractOctagon.fromInterval(b1, oct, e)
+      true
+    }
+  }
+
+  // We are *probably* no longer interested in this?
+  //
+  // property ("After trivial singlePositiveExactAssignment to C toInterval is [C,C]") = forAll {
+  //   (c: Int) => {
+  //     val a = AbstractOctagon(FunDBMInstance.funDBM.topDBM[Double](1), oct, e)
+  //     val b = AbstractOctagon(DBMUtils.singlePositiveExactAssignment(VarIndex(0), c.toDouble)(a.dbm, FunDBMInstance.funDBM), oct, e)
+  //     b.toInterval.high.head == c &&
+  //     b.toInterval.low.head == c
+  //   }
+  // }
+
+  property ("T.{x <- C} toInterval is [C,C]") {
+    forAll {
+      (c: Int) => {
+        val a = AbstractOctagon(FunDBMInstance.funDBM.topDBM[Double](1), oct, e)
+        val b = a.linearAssignment(0, LinearForm.c(c))
+        (b.toInterval.high.head == c &
+         b.toInterval.low.head == c)
+      }
+    }
+  }
+
+  property("toInterval.fromInterval == id") {
+    forAll {
+      (b1: box.Property)
+      =>
+      AbstractOctagon.fromInterval(b1, oct, e).toInterval == b1
+    }
+  }
+
+  property ("T >= _|_") {
+    forAll (GenSmallInt) {
+      case (d: Int) => {
+        val topoct =  AbstractOctagon(e.topDBM[Double](d), oct, e)
+        val botoct = AbstractOctagon(e.bottomDBM[Double](d), oct, e)
+        (topoct >= botoct)
+      }
+    }
+  }
+
+  property ("T <> _|_ for different dim") {
+    forAll (GenSmallInt) {
+      case (d: Int) => {
+        forAll (GenSmallInt) {
+          case (delta: Int) => {
+            val d1 = d + delta
+            val topoct =  AbstractOctagon(e.topDBM[Double](d), oct, e)
+            val botoct = AbstractOctagon(e.bottomDBM[Double](d1), oct, e)
+            (
+              !(topoct >= botoct) &
+                !(botoct >= topoct)
+            )
+          }
+        }
+      }
+    }
+  }
+
+  property ("T <> T for different dim") {
+    forAll (GenSmallInt) {
+      case (d: Int) => {
+        forAll (GenSmallInt) {
+          case (delta: Int) => {
+            val d1 = d + delta
+            val topoct =  AbstractOctagon(e.topDBM[Double](d), oct, e)
+            val topoct2 = AbstractOctagon(e.topDBM[Double](d1), oct, e)
+            (
+              !(topoct >= topoct2) &
+                !(topoct2 >= topoct)
+            )
+          }
+        }
+      }
+    }
+  }
+
+  property ("_|_ <> _|_ for different dim") {
+    forAll (GenSmallInt) {
+      case (d: Int) => {
+        forAll (GenSmallInt) {
+          case (delta: Int) => {
+            val d1 = d + delta
+            val botoct =  AbstractOctagon(e.bottomDBM[Double](d), oct, e)
+            val botoct2 = AbstractOctagon(e.bottomDBM[Double](d1), oct, e)
+            (!(botoct >= botoct2) &
+              (botoct2 >= botoct)
+            )
+          }
+        }
+      }
+    }
+  }
+
+  property ("T >= T") {
+    forAll (GenSmallInt) {
+      case (d: Int) => {
+        val topoct =  AbstractOctagon(e.topDBM[Double](d), oct, e)
+        val anotherTopoct = AbstractOctagon(e.bottomDBM[Double](d), oct, e)
+        (topoct >= anotherTopoct)
+      }
+    }
+  }
+
+  property ("T U _|_ = T != _|_") {
+    forAll (GenSmallInt) {
+      case (d: Int) => {
+        val topoct = AbstractOctagon(e.topDBM[Double](d), oct, e)
+        val botoct = AbstractOctagon(e.bottomDBM[Double](d), oct, e)
+        ((topoct union botoct) == topoct &
+         (topoct union botoct) != botoct)
+      }
+    }
+  }
+
+  property ("T and _|_ intersection T = _|_") {
+    forAll (GenSmallInt) {
+      case (d: Int) => {
+        val topoct = AbstractOctagon(e.topDBM[Double](d), oct, e)
+        val botoct = AbstractOctagon(e.bottomDBM[Double](d), oct, e)
+        ((topoct intersection botoct) != topoct &
+         (topoct intersection botoct) == botoct)
+      }
+    }
+  }
+
+  property ("Union of [C1,C1], [C2, C2] == [C1, C2] w/C1 < C2") {
+    forAll(GenOrderedDistinctPair) {
+      case (c1: Double, c2: Double) => {
+        val a = AbstractOctagon(e.topDBM[Double](1), oct, e)
+        val b1 = a.linearAssignment(0, c1)
+        val b2 = a.linearAssignment(0, c2)
+        val union = b1 union b2
+        (union.toInterval.isEmpty == false &
+         union.toInterval.high.head == c2 &
+         union.toInterval.low.head == c1)
+      }
+    }
+  }
+
+  property ("Intersection of [C1,C1], [C2, C2] == _|_ w/C1 < C2") {
+    forAll(GenOrderedDistinctPair) {
+      case (c1: Double, c2: Double) => {
+        val a = AbstractOctagon(e.topDBM[Double](1), oct, e)
+        val b1 = a.linearAssignment(0, c1)
+        val b2 = a.linearAssignment(0, c2)
+        val union = b1 intersection b2
+        (union.isBottom == true &
+         union.toInterval.isEmpty == true)
+      }
+    }
+  }
+
+  property ("[C1,C2] <= [C3<C1, C4>C2]") {
+    forAll(GenOrderedDistinctPair) {
+      case (x1: Double, x2: Double) =>
+        forAll(GenOrderedDistinctPair) {
+          case (x3: Double, x4: Double) => {
+            val a = AbstractOctagon(e.topDBM[Double](1), oct, e)
+            val c1 = LinearForm.c(x1);
+            val c2 = LinearForm.c(x2);
+            val posC = (x4 - x3) // x3 - x4 is certainly positive
+            assert(posC > 0)
+            val c3 = LinearForm.c(x1 - posC);
+            val c4 = LinearForm.c(x2 + posC);
+            val b1 = a.linearAssignment(0, c1)
+            val b2 = a.linearAssignment(0, c2)
+            val b3 = a.linearAssignment(0, c3)
+            val b4 = a.linearAssignment(0, c4)
+            val i1 = b1 union b2 // i1 = [x1,x2]
+            val i2 = b3 union b4 // i2 = [x3,x4]
+            (i1 <= i2)
+          }
+        }
+      }
+  }
+
+  // TODO
+  // property ("strongClosure(m) is closed") = ???
+
+}
