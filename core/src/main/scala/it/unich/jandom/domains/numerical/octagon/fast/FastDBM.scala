1355e174732905c3f271fcb4431a98885316632d
Filippo Sestini <sestini.filippo@gmail.com>
2017-07-27 21:43:49 +0200

    FastDBM: Implement 'get' in terms of inner matrix.

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/FastDBM.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/FastDBM.scala
@@ -38,38 +38,8 @@ object CFDBMInstance {
       def nOfVars[S <: DBMState, A](m: CFastDBM[M, S, A]): Int = nOfVars(m)
 
       def get[S <: DBMState, A](i: Int, j: Int)(m: CFastDBM[M, S, A])
-                               (implicit ifield: InfField[A]): Option[A] = {
-
-        def aux(m: FastDBM[M, A]): Option[A] = m match {
-          case FullDBM(dbm, dsdbm) =>
-            dsdbm.get(i, j)(dbm)
-          case DecomposedDBM(completeDBM, indepComponents, rdbm) =>
-            val vari = VarIndex(i/2)
-            val varj = VarIndex(j/2)
-            val comp = indepComponents.find(c => {
-                c.contains(vari) && c.contains(varj)
-              })
-            comp match {
-              case Some(_) => 
-                rdbm.get(i, j)(completeDBM)
-              case None =>
-                val res = if (i == j) ifield.zero else ifield.infinity
-                Some(res)
-            }
-        }
-
-        m match {
-          case BottomFast(nOfVars) =>
-            None
-          case TopFast(nOfVars) =>
-            val res = if (i == j) ifield.zero else ifield.infinity
-            Some(res)
-          case CFast(dbm) =>
-            aux(dbm)
-          case NCFast(dbm) =>
-            aux(dbm)
-        }
-      }
+                               (implicit ifield: InfField[A]): Option[A] =
+        Utils.cfastInnerMatrix(m).flatMap((inner) => ds.get(i, j)(inner))
 
       def dbmIntersection[A, R <: DBMState, S <: DBMState]
         (m1: CFastDBM[M, R, A], m2: CFastDBM[M, S, A])
