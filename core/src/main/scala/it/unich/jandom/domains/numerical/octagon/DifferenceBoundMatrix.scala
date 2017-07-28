7ac7a8480204093327472e9344e01ec961e93e1a
Filippo Sestini <sestini.filippo@gmail.com>
2017-07-28 12:15:55 +0200

    DifferenceBoundMatrix: Add implicit InfField[A] in 'update' and 'isTopDBM' and make variable-related functions closure-preserving.

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/DifferenceBoundMatrix.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/DifferenceBoundMatrix.scala
@@ -79,7 +79,7 @@ trait DifferenceBoundMatrix[M[_, _]]
   def narrowing[A, S <: DBMState, T <: DBMState]
     (m1: M[S, A], m2: M[T, A])(implicit ifield: InfField[A]): ExistsM[A]
   def bottomDBM[A](nOfVars : Int)(implicit ifield: InfField[A]) : M[Closed, A]
-  def isTopDBM[A, S <: DBMState](dbm: M[S, A]): Boolean
+  def isTopDBM[A, S <: DBMState](dbm: M[S, A])(implicit ifield: InfField[A]): Boolean
   def isBottomDBM[A, S <: DBMState](dbm: M[S, A]): Boolean
   def topDBM[A](nOfVars : Int)(implicit ifield: InfField[A]) : M[Closed, A]
   def fromFun[A] (d: Int, f: ((Int, Int) => A))(implicit ifield: InfField[A]) : M[Closed, A]
@@ -117,7 +117,9 @@ trait DifferenceBoundMatrix[M[_, _]]
   //////////////////////////////////////////////////////////////////////////////
 
   def nOfVars[S <: DBMState, A](m: M[S, A]): Int
-  def addVariable[S <: DBMState, A](dbm: M[S, A]): ExistsM[A]
-  def deleteVariable[S <: DBMState, A](v: VarIndex)(dbm: M[S, A]): ExistsM[A]
-  def mapVariables[S <: DBMState, A](f: VarIndex => Option[VarIndex])(dbm: M[S, A]): ExistsM[A]
+  def addVariable[S <: DBMState, A](dbm: M[S, A])(implicit ifield: InfField[A]): M[S, A]
+  def deleteVariable[S <: DBMState, A](v: VarIndex)(dbm: M[S, A])(implicit ifield: InfField[A]): M[S, A]
+  def mapVariables[S <: DBMState, A](f: VarIndex => Option[VarIndex])
+                                    (dbm: M[S, A])
+                                    (implicit ifield: InfField[A]): M[S, A]
 }
