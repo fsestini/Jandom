33c36f94ecd4e0439eb7346747b409f66984914e
WalkerTR <dal.bianco.davide@gmail.com>
2017-07-29 17:07:31 +0200

    HalfMatrixDenseSparse: Implement 'update'

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/HalfMatrixDenseSparseDBM.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/HalfMatrixDenseSparseDBM.scala
@@ -1,7 +1,7 @@
 package it.unich.jandom.domains.numerical.octagon.fast
 
 import it.unich.jandom.domains.numerical.octagon._
-
+import VarIndexOps._
 
 case class HalfMatrixDenseSparseDBM[A](mat: HalfMatrix[A],
                                        indeces: Seq[VarIndex],
@@ -10,14 +10,35 @@ case class HalfMatrixDenseSparseDBM[A](mat: HalfMatrix[A],
 object HalfMatrixDenseSparseInstance {
     val instance = new DenseSparseDBM[HalfMatrixDenseSparseDBM] {
 
+        private def varsToIndeces(indeces: Seq[VarIndex]): Seq[(Int, Int)] = {
+          val varIndeces = for (vi <- indeces;
+                                vj <- indeces;
+                                if vi >= vj) yield (vi, vj)
+
+          varIndeces.flatMap({ case (vi, vj) =>
+            Seq(
+              (varPlus(vi), varPlus(vj)),
+              (varPlus(vi), varMinus(vj)),
+              (varMinus(vi), varPlus(vj)),
+              (varMinus(vi), varMinus(vj))
+            )
+          })
+        }
+
         def get[A](i: Int, j: Int)(m: HalfMatrixDenseSparseDBM[A])
                   (implicit e: InfField[A]): Option[A] = Some(m.mat(i, j))
 
-        def varIndices[A](m: HalfMatrixDenseSparseDBM[A]): Seq[VarIndex] = ???
+        def varIndices[A](m: HalfMatrixDenseSparseDBM[A]): Seq[VarIndex] = m.indeces
 
         def update[A](f: (Int, Int) => A)
                      (m: HalfMatrixDenseSparseDBM[A])
-                     : HalfMatrixDenseSparseDBM[A] = ???
+                     : HalfMatrixDenseSparseDBM[A] = {
+          val elemIndeces = varsToIndeces(m.indeces)
+          val newMat = elemIndeces.foldLeft(m.mat)({ case (mat, (i, j)) =>
+              mat.update(i, j, f(i, j))
+            })
+          HalfMatrixDenseSparseDBM(newMat, m.indeces, m.dimension)
+        }
 
         def dbmUnion[A](m1: HalfMatrixDenseSparseDBM[A],
                         m2: HalfMatrixDenseSparseDBM[A])
