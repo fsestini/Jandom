23866f38d4e0cb7ad612461903c917ecfef20d74
Filippo Sestini <sestini.filippo@gmail.com>
2017-08-24 23:42:12 +0200

    Octagon: Add Lattice1

---
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Lattice1.scala
@@ -0,0 +1,36 @@
+package it.unich.jandom.domains.numerical.octagon
+
+import scala.language.higherKinds
+
+/**
+  * Created by fsestini on 7/10/17.
+  */
+
+sealed trait Ordering
+case object GT extends Ordering
+case object LT extends Ordering
+case object EQ extends Ordering
+
+trait Poset [P] {
+  def compare(x: P, y: P): Option[Ordering]
+}
+
+trait Toset [T] {
+  def compare(x: T, y: T): Ordering
+}
+
+trait Poset1[P[_]] {
+  type PosetConstraint[_]
+  def compare[A](x: P[A], y: P[A])(implicit evidence: PosetConstraint[A]): Option[Ordering]
+}
+
+trait Lattice1[L[_]] extends Poset1[L] {
+  type LatticeConstraint[_]
+  def union[A](x: L[A], y: L[A])(implicit evidence: LatticeConstraint[A]): L[A]
+  def intersection[A](x: L[A], y: L[A])(implicit evidence: LatticeConstraint[A]): L[A]
+}
+
+trait CompleteLattice1[L[_]] extends Lattice1[L] {
+  def top[A]: L[A]
+  def bottom[A]: L[A]
+}
