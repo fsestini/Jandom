ad2946182e56b1e70100dbe799d5a53706e6491c
Tobia Tesan <tobia.tesan@gmail.com>
2017-08-25 00:52:09 +0200

    Octagon: Add VarIndexOps

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Variables.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Variables.scala
@@ -1,6 +1,33 @@
 package it.unich.jandom.domains.numerical.octagon
 
+import breeze.math.Field
 // Distinguish integers used as variable indices
 case class VarIndex(i: Int) extends Ordered[VarIndex] {
   def compare(that: VarIndex) = this.i compare that.i
+}
+
+object VarIndexOps {
+  sealed trait OctaVarCoeff
+  object Positive extends OctaVarCoeff { }
+  object Negative extends OctaVarCoeff { }
+
+  def vcAsNumeral[A](c: OctaVarCoeff)(implicit ifield: Field[A]): A = c match {
+    case Positive => ifield.one
+    case Negative => ifield.inverse(ifield.one)
+  }
+
+  def varPlus(v: VarIndex): Int = 2 * v.i
+  def varMinus(v: VarIndex): Int = 2 * v.i + 1
+  def signed(i: Int): Int = if (i % 2 == 0) i + 1 else i - 1
+
+  def inverseVarPlusMinus(coeff: OctaVarCoeff, dimension: Int, i: Int): Option[VarIndex] =
+    if (0 <= i && i < dimension && toIndexAndCoeff(i)._2 == coeff)
+      Some(toIndexAndCoeff(i)._1) else None
+  def toIndexAndCoeff(i: Int): (VarIndex, OctaVarCoeff) =
+    if (i % 2 == 0) (VarIndex(i / 2), Positive)
+    else (VarIndex((i - 1) / 2), Negative)
+  def fromIndexAndCoeff(vi: VarIndex, c: OctaVarCoeff): Int = c match {
+    case Positive => varPlus(vi)
+    case Negative => varMinus(vi)
+  }
 }
