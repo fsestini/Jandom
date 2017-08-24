87ecfd481f3a4a67581d545c2d96e7bd29e75783
Tobia Tesan <tobia.tesan@gmail.com>
2017-08-25 00:00:59 +0200

    Octagon: Add OctagonDomain

---
+++ core/src/main/scala/it/unich/jandom/domains/numerical/OctagonDomain.scala
@@ -0,0 +1,32 @@
+package it.unich.jandom.domains.numerical
+import it.unich.jandom.domains.WideningDescription
+import it.unich.jandom.domains.numerical.octagon._
+import scala.language.higherKinds
+
+class OctagonDomain[M[_,_]] private[numerical] (
+  e: DifferenceBoundMatrix[M] { type PosetConstraint[A] = InfField[A]})
+    extends NumericalDomain {
+
+  // Copied from BoxDoubleDomain.
+  val widenings = Seq(WideningDescription.default[Property])
+
+  /**
+    * @inheritdoc
+    * @note @inheritdoc
+    * @throws $ILLEGAL
+    */
+  def top(dimension: Int): Property = AbstractOctagon(e.topDBM(VarCount(dimension)), this, e)
+
+  /**
+    * @inheritdoc
+    * @note @inheritdoc
+    * @throws $ILLEGAL
+    */
+  def bottom(dimension: Int): Property = AbstractOctagon(e.bottomDBM(VarCount(dimension)), this, e)
+
+  type Property = AbstractOctagon[OctagonDomain[M], M]
+}
+
+object OctagonDomain {
+  def apply() = new OctagonDomain(FunDBMInstance.funDBM)
+}
