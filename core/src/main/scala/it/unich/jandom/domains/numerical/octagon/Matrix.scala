485313ee121f0ef3f679bc93182a7bd947579f24
Filippo Sestini <sestini.filippo@gmail.com>
2017-08-24 23:08:21 +0200

    Octagon: Add Matrix

---
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/Matrix.scala
@@ -0,0 +1,14 @@
+package it.unich.jandom.domains.numerical.octagon
+
+import scala.language.higherKinds
+import scalaz._
+
+trait Matrix[M[_]] // extends Functor[M] with Foldable[M] {
+  extends Foldable[M] {
+
+  def update[A](i: Int, j: Int, x: A)(m: M[A]): M[A]
+  def update[A](updater: (Int, Int) => A)(m: M[A]): M[A]
+  def get[A](i: Int, j: Int)(m: M[A]): A
+  def combine[A, B, C](f: (A, B) => C)(ma: M[A], mb: M[B]): M[C]
+  def pure[A](dimension: Dimension, x: A): M[A]
+}
