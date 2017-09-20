4d111b7a9629be8e583fa34b6e15670a9bfa7939
Filippo Sestini <sestini.filippo@gmail.com>
2017-09-20 12:41:58 +0200

    FastDBM: Fill fromFun.

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/FastDBM.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/FastDBM.scala
@@ -94,8 +94,10 @@ object CFDBMInstance {
       def bottomDBM[A](nOfVars: VarCount)(implicit ifield: InfField[A]):
           CFastDBM[M, SM, Closed, A] = BottomFast(nOfVars)
 
-      def fromFun[A](d: Dimension, f: ((Int, Int) => A))(implicit ifield: InfField[A]): CFastDBM[M, SM, Closed, A] =
-        CFast(FullDBM(mev.ds.update(f)(mev.dec.pure(dimToVarCount(d), ifield.infinity)), mev))
+      def fromFun[A](d: Dimension, f: ((Int, Int) => A))
+        (implicit ifield: InfField[A]): CFastDBM[M, SM, Closed, A] =
+        CFast(FullDBM(mev.ds.update(f)(
+          mev.dec.pure(dimToVarCount(d), ifield.infinity)), mev))
 
       def flipVar[S <: DBMState, A](vi: VarIndex)(m: CFastDBM[M, SM, S, A])
                                    (implicit ifield: InfField[A]): CFastDBM[M, SM, S, A] =
